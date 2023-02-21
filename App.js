53ae663cda6850e8107f73312e8b7629f70436d0
AshtonAshley01 <ashtonashley001@gmail.com>
2023-02-22 00:19:56 +0530

    just added some features

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button, Alert, TouchableOpacity } from 'react-native';
+import PresentationalComponent from './components/PresentationalComponents'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+   state = {
+      myState: 'Hey There! THis is a test app'
+   }
+   updateState = () => {
+      this.setState({ myState: 'The state is updated' })
+   }
+   render() {
+      return (
+         <SafeAreaView style={styles.container}>
+            <PresentationalComponent myState = {this.state.myState} updateState = {this.updateState}/>
+
+            <Button title="Press me" color="red" onPress={() => Alert.alert('Simple Button pressed')}/>
+
+            <TouchableOpacity onPress={() => Alert.alert("Touchable Button Pressed")} style={styles.button}>
+            <Text style={styles.buttonText}>Click Me</Text>
+            </TouchableOpacity>
+         </SafeAreaView>
+      );
+   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
+    justifyContent: 'centre',
   },
-});
+  button: {
+    backgroundColor: "gray",
+    padding: 20,
+    borderRadius: 10
+  },
+  buttonText: {
+    color: "white"
+  },
+})
