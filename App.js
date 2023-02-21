f595f92de3ede6b573078a87beb45997a8717223
AshtonAshley01 <ashtonashley001@gmail.com>
2023-02-22 00:51:27 +0530

    just added some paddingTop features blah blah

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button, Alert, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Alert, TouchableOpacity, Platform, StatusBar } from 'react-native';
 import PresentationalComponent from './components/PresentationalComponents'
 
 export default class App extends React.Component {
    state = {
-      myState: 'Hey There! THis is a test app'
+      myState: 'Hey There! THis is a test app '
    }
    updateState = () => {
       this.setState({ myState: 'The state is updated' })
@@ -28,7 +28,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'dodgerblue',
-    justifyContent: 'centre',
+    // justifyContent: 'centre',
+    // alignItems: "centre",
+    // marginTop: Platform.OS === "android" ? 10 : 0,
+    // paddingTop: Platform.OS === "ios" ? 500 : 0,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight:20,
   },
   button: {
     backgroundColor: "gray",
